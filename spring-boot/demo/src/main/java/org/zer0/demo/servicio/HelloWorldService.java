cb01ab306853bca6708130d45432a7040465d21a
Joseph <ip.zer0@gmail.com>
2018-02-11 17:29:37 -0500

    .

--- spring-boot/demo/src/main/java/org/zer0/demo/servicio/HelloWorldService.java
+++ spring-boot/demo/src/main/java/org/zer0/demo/servicio/HelloWorldService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldService {
 	
 	@RequestMapping("/saludo")
-	public String saludo(@RequestParam(value="name",defaultValue="Mundo") String name ){
+	public String saludo(@RequestParam(value="name", defaultValue="Mundo") String name ){
 		return "Hola "+name;
 	}
 }
