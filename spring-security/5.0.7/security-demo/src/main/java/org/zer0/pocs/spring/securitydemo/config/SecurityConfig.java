1d10374f4620ab30df813aa9e7f7ec5e3ac6edb7
Joseph Mena <ip.zer0@gmail.com>
2018-08-05 21:43:43 -0700

    version con spring security

--- spring-security/5.0.7/security-demo/src/main/java/org/zer0/pocs/spring/securitydemo/config/SecurityConfig.java
+++ spring-security/5.0.7/security-demo/src/main/java/org/zer0/pocs/spring/securitydemo/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package org.zer0.pocs.spring.securitydemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,7 +13,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity security) throws Exception {
-		security.authorizeRequests().antMatchers("/*").permitAll();
-		security.httpBasic().disable();
+		security.authorizeRequests()
+					.antMatchers("/index").permitAll()
+					.antMatchers("/user/index").hasRole("USER")
+				.and()
+				.formLogin()
+					.loginPage("/login");
+		//security.httpBasic().disable();
 	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+				.withUser("user").password("password").roles("USER");
+	}
+	
 }
