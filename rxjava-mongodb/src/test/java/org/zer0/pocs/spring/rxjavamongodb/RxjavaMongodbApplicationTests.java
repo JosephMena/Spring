6ed83e2b9b7afb2ee930b31ede0ea5f16845b594
Joseph Mena <ip.zer0@gmail.com>
2018-08-24 18:06:00 -0500

    proyecto vacio con Spring boot+rxjava2+mongodb

---
+++ rxjava-mongodb/src/test/java/org/zer0/pocs/spring/rxjavamongodb/RxjavaMongodbApplicationTests.java
@@ -0,0 +1,16 @@
+package org.zer0.pocs.spring.rxjavamongodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RxjavaMongodbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
