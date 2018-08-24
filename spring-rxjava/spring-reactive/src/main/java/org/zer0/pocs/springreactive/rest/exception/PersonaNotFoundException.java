9ebbfcaca9d185138619fadf7a6f35466d95c411
Joseph Mena <ip.zer0@gmail.com>
2018-08-24 17:32:20 -0500

    Agregando Soporte con Optional y metodos funcionales

---
+++ spring-rxjava/spring-reactive/src/main/java/org/zer0/pocs/springreactive/rest/exception/PersonaNotFoundException.java
@@ -0,0 +1,15 @@
+package org.zer0.pocs.springreactive.rest.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class PersonaNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 5522368114807396802L;
+
+	public PersonaNotFoundException(String personaId) {
+		super("could not find user '" + personaId + "'.");
+	}
+	
+}
